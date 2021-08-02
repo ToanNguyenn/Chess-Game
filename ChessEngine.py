91d85b7e1831bd69a2018ba3e7e2bf6a4e15ec0d
ToanNguyenn <88270091+ToanNguyenn@users.noreply.github.com>
2021-08-02 16:28:48 +0700

    Update movement rules

--- ChessEngine.py
+++ ChessEngine.py
@@ -47,22 +47,98 @@ class GameState():
         return moves
 
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove:
+            if self.board[r-1][c] == '--':
+                moves.append(Move((r,c), (r-1,c), self.board))
+                if r == 6 and self.board[r-2][c] == '--':
+                    moves.append(Move((r,c), (r-2,c), self.board))
+
+            if c-1 >= 0:
+                if self.board[r-1][c-1][0] == 'b':
+                    moves.append((Move((r, c), (r-1, c-1), self.board)))
+
+            if c+1 <= 7:
+                if self.board[r-1][c+1][0] == 'b':
+                    moves.append((Move((r, c), (r-1, c+1), self.board)))
+
+        if not self.whiteToMove:
+            if self.board[r+1][c] == '--':
+                moves.append(Move((r, c), (r+1,c), self.board))
+                if r == 1 and self.board[r+2][c] == '--':
+                    moves.append(Move((r, c), (r+2, c), self.board))
+
+            if c-1 >= 0:
+                if self.board[r+1][c-1][0] == 'w':
+                    moves.append(Move((r, c), (r+1, c-1), self.board))
+
+            if c+1 <= 7:
+                if self.board[r+1][c+1][0] == 'w':
+                    moves.append(Move((r, c), (r+1, c+1), self.board))
 
     def getRookMoves(self, r, c, moves):
-        pass
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        enemy = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1,8):
+                endRow = r + d[0]*i
+                endCol = c + d[1]*i
+                if 0 <= endCol < 8 and 0 <= endRow < 8: #onboard
+                    endPieces = self.board[endRow][endCol]
+                    if endPieces == "--": # if no piece
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPieces[0] == enemy: # if enemy piece
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else: # if friend piece
+                        break
+                else:   #out board
+                    break
 
     def getBishopMoves(self, r, c, moves):
-        pass
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemy = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                endRow = r + d[0]*i
+                endCol = c + d[1]*i
+                if 0 <= endCol < 8 and 0 <= endRow < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemy:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
 
     def getKingMoves(self, r, c, moves):
-        pass
+        directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        ally = "w" if self.whiteToMove else "b"
+        for d in directions:
+            endRow = r + d[0]
+            endCol = c + d[1]
+            if 0 <= endCol < 8 and 0 <= endRow < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece != ally:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
     def getQueenMoves(self, r, c, moves):
-        pass
+        self.getRookMoves(r, c, moves)
+        self.getBishopMoves(r, c, moves)
 
     def getKnightMoves(self, r, c, moves):
-        pass
+        directions = ((-2, -1), (-2, 1), (-1, -2), (-1, 2),(1, -2), (1, 2), (2, -1), (2, 1))
+        ally = "w" if self.whiteToMove else "b"
+        for d in directions:
+            endRow = r + d[0]
+            endCol = c + d[1]
+            if 0 <= endCol < 8 and 0 <= endRow < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != ally:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+
 
 class Move:
     # maps key to value
